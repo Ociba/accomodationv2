a5c7dcdfd0fe7aeb2db5de8ce49407ce6cf0c4bc
Ociba James <ocibajames@gmail.com>
2022-03-17 17:29:04 +0300

    changes

---
+++ app/Http/Livewire/BoysRevenuePerTicket.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BoysRevenuePerTicket extends Component
+{
+    public function render()
+    {
+        return view('livewire.boys-revenue-per-ticket');
+    }
+}
